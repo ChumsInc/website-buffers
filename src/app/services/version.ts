482e72cb8efc7204e59335d8e95d1a45885428dd
Steve Montgomery <steve@chums.com>
2025-02-26 13:47:34 -0700

    fix: -- api/version.ts - prevent caching the request

--- src/app/services/version.ts
+++ src/app/services/version.ts
@@ -8,7 +8,10 @@ interface VersionResponse {
 export const versionApi = api.injectEndpoints({
     endpoints: (build) => ({
         getVersion: build.query<string, void>({
-            query: () => '/apps/website-buffers/package.json',
+            query: () => ({
+                url: '/apps/website-buffers/package.json',
+                cache: 'no-cache',
+            }),
             transformResponse: (response: VersionResponse) => response.version ?? 'N/A',
             providesTags: () => [{type: 'Version'}]
         })
